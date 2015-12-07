c16c735d6f3aab45e243e860961a617d348fded1
bartoszka1996 <bartoszka1996@gmail.com>
2015-12-07 16:57:00 +0200

    sdxcvds
    sdfdsf

--- laborki4/lab4zad3.cpp
+++ laborki4/lab4zad3.cpp
@@ -1,34 +1,47 @@
 #include <iostream>
 #include <stack>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    stack<int> s1;
-    stack<int> s2;
-    cout<<"Podawaj po kolei kolejne cyfry pierwszej liczby, zaczynajac od lewej strony, ktora chcesz dodac. Gdy liczba sie skonczy podaj dowolna liczbe wieksza od 9 lub mniejsza od 0."<<endl;
-    int x;
+    stack<char> s1;
+    stack<char> s2;
+    char x;
+    cout<<"Podaj pierwsza liczbe: ";
     do {
-        cout<<"Podaj cyfre: ";
-        cin>>x;
+        x = getchar();
+        x -=48;
         if(x>=0 && x<=9) s1.push(x);
     }
     while (x>=0 && x<=9);
-    cout<<"Podawaj po kolei kolejne cyfry drugiej liczby, zaczynajac od lewej strony, ktora chcesz dodac. Gdy liczba sie skonczy podaj dowolna liczbe wieksza od 9 lub mniejsza od 0."<<endl;
+    cout<<"Podaj druga liczbe: ";
     do {
-        cout<<"Podaj cyfre: ";
-        cin>>x;
+        x = getchar();
+        x -=48;
         if(x>=0 && x<=9) s2.push(x);
     }
     while (x>=0 && x<=9);
-    stack<int> s3;
-    int rozmiar1 = s1.size();
-    int rozmiar2 = s2.size();
-    int rozmiar = (rozmiar1>rozmiar2)?rozmiar1:rozmiar2;
-    //cout<<rozmiar1<<endl<<rozmiar2<<endl<<rozmiar<<endl;
-    int r = 0;
-    int a,b,s;
+    /*cout<<"Podawaj po kolei kolejne cyfry pierwszej liczby, zaczynajac od lewej strony, ktora chcesz dodac. Gdy liczba sie skonczy podaj dowolna litere."<<endl;
+    do {
+        cout<<"Podaj cyfre: ";
+        cin>>x;
+        x -=48;
+        if(x>=0 && x<=9) s1.push(x);
+    }
+    while (x>=0 && x<=9);
+    cout<<"Podawaj po kolei kolejne cyfry drugiej liczby, zaczynajac od lewej strony, ktora chcesz dodac. Gdy liczba sie skonczy podaj dowolna litere."<<endl;
+    do {
+        cout<<"Podaj cyfre: ";
+        cin>>x;
+        x-=48;
+        if(x>=0 && x<=9) s2.push(x);
+    }
+    while (x>=0 && x<=9);*/
+    stack<char> s3;
+    char r = 0;
+    char a,b,s;
     while (!s1.empty()||!s2.empty()||r==1){
         if(!s1.empty()) a = s1.top();
         else a = 0;
@@ -44,28 +57,16 @@ int main()
         if(!s1.empty()) s1.pop();
         if(!s2.empty()) s2.pop();
     }
-    //stack<int> s4;
-    x=0;
-    /*while (!s3.empty()){
-        x = s3.top();
-        s4.push(x);
-        s3.pop();
-    }*/
     x=0;
     cout<<"Suma liczb po dodaniu wynosi: ";
+    if(s3.empty()) cout<<0;
     while (!s3.empty()){
         x = s3.top();
+        x+=48;
         s3.pop();
         cout<<x;
     }
     cout<<endl;
-    
-    
-    
-    
-    
-    
-    
     
     
     cout<<"Naciśnij kilkakrtonie ENTER, aby wyjść...";//pisze na linuxie, wiec komenda 'system("pause");' nie dziala
